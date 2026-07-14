c0b7256590196d3144f1a24d45471f37d77e8b03
Flavio <flavio_125@hotmail.com>
2026-07-14 13:13:39 -0300

    Implementa CRUD completo da entidade Cliente

--- src/main/java/gestao_cadastros/controller/ClienteController.java
+++ src/main/java/gestao_cadastros/controller/ClienteController.java
@@ -2,7 +2,16 @@ package gestao_cadastros.controller;
 
 import gestao_cadastros.entity.Cliente;
 import gestao_cadastros.service.ClienteService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +29,34 @@ public class ClienteController {
     @PostMapping
     public Cliente salvar(@RequestBody Cliente cliente) {
         return clienteService.salvar(cliente);
+    }
+    
+    @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+    	
+    	Optional<Cliente> cliente = clienteService.buscarPorId(id);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    	   	
+    }
+    
+    @PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return clienteService.atualizar(id, cliente);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+    	clienteService.excluir(id);
     }
 
 }
--- src/main/java/gestao_cadastros/service/ClienteService.java
+++ src/main/java/gestao_cadastros/service/ClienteService.java
@@ -1,5 +1,8 @@
 package gestao_cadastros.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import gestao_cadastros.entity.Cliente;
@@ -16,5 +19,36 @@ public class ClienteService {
     
     public Cliente salvar(Cliente cliente) {
         return clienteRepository.save(cliente);
+    }
+    
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+    
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+    
+    public Cliente atualizar(Long id, Cliente cliente) {
+    	
+    	Optional<Cliente> clienteExistente = clienteRepository.findById(id);
+    	
+    	if (clienteExistente.isEmpty()) {
+   	
+    		throw new RuntimeException("Cliente não encontrado");
+    	}
+    	
+    	Cliente clienteSalvo = clienteExistente.get();
+    	
+    	clienteSalvo.setNome(cliente.getNome());
+    	clienteSalvo.setCpf(cliente.getCpf());
+    	clienteSalvo.setEmail(cliente.getEmail());
+    	clienteSalvo.setTelefone(cliente.getTelefone());
+    	
+    	return clienteRepository.save(clienteSalvo);
+    }
+    
+    public void excluir(Long id) {
+    	clienteRepository.deleteById(id);
     }
 }
